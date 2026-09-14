8ba8b5f112a4c4d58d4af702f3938adbe2e338b6
Emmanuel Manyara <manyaraemmanuel7@gmail.com>
2026-09-14 23:03:51 +0300

    Force IPv4 for Aiven MySQL

--- api/[...path].js
+++ api/[...path].js
@@ -1,13 +1,13 @@
 const mysql = require("mysql2/promise");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
+  family: 4,
   ssl: {
     rejectUnauthorized: false,
   },
