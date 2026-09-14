1043254c817dd8678bf6a4619b284e11e559462e
Emmanuel Manyara <manyaraemmanuel7@gmail.com>
2026-09-14 19:06:31 +0300

    Enable SSL for Aiven MySQL

--- netlify/functions/api.cjs
+++ netlify/functions/api.cjs
@@ -8,6 +8,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 exports.handler = async (event) => {
